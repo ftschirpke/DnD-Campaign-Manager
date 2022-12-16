090fafb813ea413a84b278a20fa324dcc32e11ce
Friedrich Tschirpke <tschirpf@student.hu-berlin.de>
2022-12-16 20:46:22 +0100

    added: spell type

--- src/models/spell.hpp
+++ src/models/spell.hpp
@@ -24,9 +24,29 @@ enum MagicSchool {
     TRANSMUTATION
 };
 
+enum SpellLevel {
+    CANTRIP = 0,
+    LEVEL1 = 1,
+    LEVEL2 = 2,
+    LEVEL3 = 3,
+    LEVEL4 = 4,
+    LEVEL5 = 5,
+    LEVEL6 = 6,
+    LEVEL7 = 7,
+    LEVEL8 = 8,
+    LEVEL9 = 9,
+};
+
+struct SpellType {
+    MagicSchool magic_school;
+    SpellLevel level;
+    bool is_ritual;
+};
+
 class Spell {
 public:
     const std::string name;
+    const SpellType type;
     Spell(const std::string& name);
 };
 
