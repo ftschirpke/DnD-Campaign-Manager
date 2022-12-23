3dd2a2a59ab612713a7c1cfe9f0e5c7b8dc80c39
Friedrich Tschirpke <tschirpf@student.hu-berlin.de>
2022-12-23 05:14:29 +0100

    feat: full clang-format checker as bash script

--- .github/workflows/format_file.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-clang-format -style=file -output-replacements-xml $1 | grep -c "<replacement " >/dev/null
-
-if [ $? -eq 0 ]; then
-    echo "formatted incorrectly: $1"
-    exit 1
-fi
