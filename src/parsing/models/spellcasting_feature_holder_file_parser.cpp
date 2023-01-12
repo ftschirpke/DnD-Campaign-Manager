315719e2cb547a7505afd112e39a20cec2b87f5e
Friedrich Tschirpke <tschirpf@student.hu-berlin.de>
2023-01-12 01:31:36 +0100

    feat: parse spellcasting

--- src/parsing/models/spellcasting_feature_holder_file_parser.cpp
+++ src/parsing/models/spellcasting_feature_holder_file_parser.cpp
@@ -8,6 +8,7 @@
 
 #include <nlohmann/json.hpp>
 
+#include "basic_mechanics/abilities.hpp"
 #include "models/spellcasting/preparation_spellcasting.hpp"
 #include "models/spellcasting/spellcasting.hpp"
 #include "models/spellcasting/spells_known_spellcasting.hpp"
@@ -21,10 +22,55 @@ void dnd::SpellcastingFeatureHolderFileParser::parseSize20Array(
     if (!attribute_json.is_array() || attribute_json.size() != 20) {
         throw invalid_attribute(filepath, attribute_name, "should be an array of 20 numbers.");
     }
-    output = attribute_json.get<std::array<int, 20>>();
+    if (attribute_json.contains(attribute_name)) {
+        output = attribute_json.get<std::array<int, 20>>();
+    } else {
+        output = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    }
 }
 
-void dnd::SpellcastingFeatureHolderFileParser::parseSpellcasting() {}
+void dnd::SpellcastingFeatureHolderFileParser::parseSpellcasting() {
+    const nlohmann::json& spellcasting_json = json_to_parse.at("spellcasting");
+    ability = spellcasting_json.at("ability").get<std::string>();
+    if (!isAbility(ability)) {
+        throw invalid_attribute(filepath, "spellcasting:attribute", "is not an ability");
+    }
+    ritual_casting = spellcasting_json.at("ritual_casting").get<bool>();
+
+    bool has_spells_known = spellcasting_json.contains("spells_known");
+    bool has_preparation_caster = spellcasting_json.contains("preparation_caster");
+    if (has_spells_known && has_preparation_caster) {
+        throw invalid_attribute(
+            filepath, "spellcasting:spells_known/preparation_caster", "attributes are mutally exclusive."
+        );
+    } else if (has_spells_known) {
+        spellcasting_type = SpellcastingType::SPELLS_KNOWN;
+        parseSize20Array(spellcasting_json, "spells_known", spells_known);
+    } else if (has_preparation_caster) {
+        spellcasting_type = SpellcastingType::PREPARATION;
+        std::string preparation_spellcasting_type_str = spellcasting_json.at("preparation_caster").get<std::string>();
+        if (preparation_spellcasting_type_str == "half") {
+            preparation_spellcasting_type = PreparationSpellcastingType::HALF;
+        } else if (preparation_spellcasting_type_str == "full") {
+            preparation_spellcasting_type = PreparationSpellcastingType::HALF;
+        } else {
+            throw invalid_attribute(filepath, "spellcasting:preparation_caster", "must be \"half\" or \"full\".");
+        }
+    } else {
+        throw attribute_missing(filepath, "spellcasting:spells_known or spellcasting:preparation_caster");
+    }
+
+    parseSize20Array(spellcasting_json, "cantrips_known", cantrips_known);
+    parseSize20Array(spellcasting_json, "level1_slots", spell_slots[0]);
+    parseSize20Array(spellcasting_json, "level2_slots", spell_slots[1]);
+    parseSize20Array(spellcasting_json, "level3_slots", spell_slots[2]);
+    parseSize20Array(spellcasting_json, "level4_slots", spell_slots[3]);
+    parseSize20Array(spellcasting_json, "level5_slots", spell_slots[4]);
+    parseSize20Array(spellcasting_json, "level6_slots", spell_slots[5]);
+    parseSize20Array(spellcasting_json, "level7_slots", spell_slots[6]);
+    parseSize20Array(spellcasting_json, "level8_slots", spell_slots[7]);
+    parseSize20Array(spellcasting_json, "level9_slots", spell_slots[8]);
+}
 
 std::unique_ptr<dnd::Spellcasting> dnd::SpellcastingFeatureHolderFileParser::retrieveSpellcasting() {
     switch (spellcasting_type) {
@@ -35,4 +81,5 @@ std::unique_ptr<dnd::Spellcasting> dnd::SpellcastingFeatureHolderFileParser::ret
         default:
             throw std::logic_error("Parsing of spellcasting type not implemented.");
     }
+    // TODO: add cantrips_known, spell_slots (and spells_known)
 }
