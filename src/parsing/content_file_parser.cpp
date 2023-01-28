863129024520b3569101a7d0f3a6e0cf5fbeac95
Friedrich Tschirpke <tschirpf@student.hu-berlin.de>
2023-01-28 13:47:07 +0100

    feat: content file parser allows configuring subparsers

--- src/parsing/content_file_parser.cpp
+++ src/parsing/content_file_parser.cpp
@@ -27,5 +27,8 @@ bool dnd::ContentFileParser::openJSON(const std::filesystem::directory_entry& fi
         std::cerr << "Warning: Error occured while parsing " << filepath << ":\n" << e.what() << '\n';
         return false;
     }
+    configureSubparsers();
     return true;
 }
+
+void dnd::ContentFileParser::configureSubparsers() {}
--- src/parsing/content_file_parser.hpp
+++ src/parsing/content_file_parser.hpp
@@ -7,6 +7,8 @@
 
 #include <nlohmann/json.hpp>
 
+#include "parsing_types.hpp"
+
 namespace dnd {
 
 // parse a simple, optional attribute from a json into the output variable
@@ -15,13 +17,33 @@ void parseOptional(const nlohmann::json& json_to_parse, const char* attribute_na
 
 class ContentFileParser {
 public:
+    /**
+     * @brief Opens a json file to be parsed and deserialises the JSON.
+     * @param filepath the file to open
+     * @return "true" if opening and deserialising was successful, "false" otherwise
+     */
     virtual bool openJSON(const std::filesystem::directory_entry& filepath);
+    /**
+     * @brief Parses JSON content file.
+     */
     virtual void parse() = 0;
+    /**
+     * @brief Checks whether parsed content is valid.
+     * @return "true" if content is valid, "false" otherwise
+     */
     virtual bool validate() const = 0;
+    /**
+     * @brief Saves the parsed content.
+     */
     virtual void saveResult() = 0;
 protected:
     nlohmann::json json_to_parse;
     std::filesystem::path filepath;
+    /**
+     * @brief This function is automatically called after successfully opening the JSON file to set the properties of
+     * subparsers if necessary.
+     */
+    virtual void configureSubparsers();
 };
 
 template <typename T>
