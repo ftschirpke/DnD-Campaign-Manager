7879fcf7e9f1a99bebb392eac0b9d5fbca94a2f5
Friedrich Tschirpke <tschirpf@student.hu-berlin.de>
2023-02-06 11:57:31 +0100

    fix: remove trivially destructible check

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,8 @@
 #include "dnd_config.hpp"
 
-#include <iostream>
-#include <type_traits>
-
 #include "launcher.hpp"
 
 int main(int argc, char** argv) {
-    std::cout << std::boolalpha << std::is_trivially_destructible_v<dnd::Measurer> << '\n';
     DND_START_MEASURING_SESSION("LAUNCH", "debug/runtime_measurement_results.json");
     { DND_MEASURE_SCOPE("FIRST"); }
     int rv = dnd::launch(argc, argv);
