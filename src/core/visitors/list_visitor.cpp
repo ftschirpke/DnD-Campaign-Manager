d04ded4dc24e9e665f3d5a7d59eb299be1c3645d
Friedrich Tschirpke <friedrich.tschirpke@student.hu-berlin.de>
2023-06-01 18:39:35 +0200

    fix: remove wrong fmt::format usage

--- src/core/visitors/list_visitor.cpp
+++ src/core/visitors/list_visitor.cpp
@@ -20,42 +20,42 @@
 
 void dnd::ListVisitor::visit(const Character* character_ptr) {
     string_list.emplace_back(fmt::format(
-        "%s (Character) - %s - Level %d %s", character_ptr->name, character_ptr->race_ptr->name,
+        "{} (Character) - {} - Level {} {}", character_ptr->name, character_ptr->race_ptr->name,
         character_ptr->getLevel(), character_ptr->class_ptr->name
     ));
 }
 
 void dnd::ListVisitor::visit(const CharacterClass* character_class_ptr) {
-    string_list.emplace_back(fmt::format("%s (Class)", character_class_ptr->name));
+    string_list.emplace_back(fmt::format("{} (Class)", character_class_ptr->name));
 }
 
 void dnd::ListVisitor::visit(const CharacterSubclass* character_subclass_ptr) {
     string_list.emplace_back(
-        fmt::format("%s (Subclass %s)", character_subclass_ptr->name, character_subclass_ptr->class_name)
+        fmt::format("{} ({} Subclass)", character_subclass_ptr->name, character_subclass_ptr->class_name)
     );
 }
 void dnd::ListVisitor::visit(const CharacterRace* character_race_ptr) {
-    string_list.emplace_back(fmt::format("%s (Race)", character_race_ptr->name));
+    string_list.emplace_back(fmt::format("{} (Race)", character_race_ptr->name));
 }
 
 void dnd::ListVisitor::visit(const CharacterSubrace* character_subrace_ptr) {
     string_list.emplace_back(
-        fmt::format("%s (Subrace %s)", character_subrace_ptr->name, character_subrace_ptr->race_name)
+        fmt::format("{} ({} Subrace)", character_subrace_ptr->name, character_subrace_ptr->race_name)
     );
 }
 
 void dnd::ListVisitor::visit(const Item* item_ptr) {
-    string_list.emplace_back(fmt::format("%s (Item)", item_ptr->name));
+    string_list.emplace_back(fmt::format("{} (Item)", item_ptr->name));
 }
 
 void dnd::ListVisitor::visit(const Spell* spell_ptr) {
-    string_list.emplace_back(fmt::format("%s (Spell)", spell_ptr->name));
+    string_list.emplace_back(fmt::format("{} (Spell)", spell_ptr->name));
 }
 
 void dnd::ListVisitor::visit(const Feature* feature_ptr) {
-    string_list.emplace_back(fmt::format("%s (Feature)", feature_ptr->name));
+    string_list.emplace_back(fmt::format("{} (Feature)", feature_ptr->name));
 }
 
 void dnd::ListVisitor::visit(const Choosable* choosable_ptr) {
-    string_list.emplace_back(fmt::format("%s (Choosable)", choosable_ptr->name));
+    string_list.emplace_back(fmt::format("{} (Choosable)", choosable_ptr->name));
 }
