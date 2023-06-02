e27eb0c6e328d39770f24344765f3139f175a57a
Friedrich Tschirpke <friedrich.tschirpke@student.hu-berlin.de>
2023-06-03 00:45:51 +0200

    feat: display spells

--- src/gui_app/content_visitors/display_visitor.cpp
+++ src/gui_app/content_visitors/display_visitor.cpp
@@ -15,6 +15,7 @@
 #include "core/models/character_race.hpp"
 #include "core/models/character_subclass.hpp"
 #include "core/models/character_subrace.hpp"
+#include "core/models/content_piece.hpp"
 #include "core/models/effect_holder/choosable.hpp"
 #include "core/models/effect_holder/feature.hpp"
 #include "core/models/item.hpp"
@@ -25,6 +26,36 @@
 static const ImVec2 cell_padding = ImVec2(5, 5);
 static const ImGuiTableFlags content_table_flags = ImGuiTableFlags_NoBordersInBodyUntilResize;
 static const float first_column_width = 150;
+
+static void label(const char* label) {
+    ImGui::TableNextRow();
+    ImGui::TableSetColumnIndex(0);
+    ImGui::Text("%s", label);
+    ImGui::TableSetColumnIndex(1);
+}
+
+static void source(const dnd::ContentPiece* content_piece_ptr) {
+    ImGui::TableNextRow();
+    ImGui::TableSetColumnIndex(0);
+    ImGui::Text("Source:");
+    ImGui::Separator();
+    ImGui::TableSetColumnIndex(1);
+    ImGui::TextWrapped("%s", content_piece_ptr->source_file_path.string().c_str());
+    ImGui::Separator();
+}
+
+static void begin_content_table(const dnd::ContentPiece* content_piece_ptr) {
+    std::string table_id = fmt::format("{}_table", content_piece_ptr->name);
+    ImGui::PushStyleVar(ImGuiStyleVar_CellPadding, cell_padding);
+    ImGui::BeginTable(table_id.c_str(), 2, content_table_flags);
+    ImGui::TableSetupColumn("Labels", ImGuiTableColumnFlags_WidthFixed, first_column_width);
+    ImGui::TableSetupColumn("Values");
+}
+
+static void end_content_table() {
+    ImGui::EndTable();
+    ImGui::PopStyleVar();
+}
 
 void dnd::DisplayVisitor::visit(const Character* character_ptr) {
     DND_UNUSED(character_ptr);
@@ -57,68 +88,51 @@ void dnd::DisplayVisitor::visit(const Item* item_ptr) {
 }
 
 void dnd::DisplayVisitor::visit(const Spell* spell_ptr) {
-    DND_UNUSED(spell_ptr);
-    // TODO
+    begin_content_table(spell_ptr);
+
+    label("Type:");
+    ImGui::Text("Spell");
+    source(spell_ptr);
+    label("Spell Type:");
+    ImGui::Text("%s", spell_ptr->type.str().c_str());
+    label("Casting Time:");
+    ImGui::Text("%s", spell_ptr->casting_time.c_str());
+    label("Range:");
+    ImGui::Text("%s", spell_ptr->range.c_str());
+    label("Components:");
+    ImGui::TextWrapped("%s", spell_ptr->components.str().c_str());
+    label("Duration:");
+    ImGui::Text("%s", spell_ptr->duration.c_str());
+
+
+    label("Description:");
+    display_formatted_text(spell_ptr->description);
+
+    end_content_table();
 }
 
 void dnd::DisplayVisitor::visit(const Feature* feature_ptr) {
-    std::string table_id = fmt::format("{}_table", feature_ptr->name);
-    ImGui::PushStyleVar(ImGuiStyleVar_CellPadding, cell_padding);
-    if (ImGui::BeginTable(table_id.c_str(), 2, content_table_flags)) {
-        ImGui::TableSetupColumn("Labels", ImGuiTableColumnFlags_WidthFixed, first_column_width);
-        ImGui::TableSetupColumn("Values");
+    begin_content_table(feature_ptr);
 
-        ImGui::TableNextRow();
-        ImGui::TableSetColumnIndex(0);
-        ImGui::Text("Type:");
-        ImGui::TableSetColumnIndex(1);
-        ImGui::Text("Feature");
+    label("Type:");
+    ImGui::Text("Feature");
+    source(feature_ptr);
+    label("Description:");
+    display_formatted_text(feature_ptr->description);
 
-        ImGui::TableNextRow();
-        ImGui::TableSetColumnIndex(0);
-        ImGui::Text("Source:");
-        ImGui::TableSetColumnIndex(1);
-        display_formatted_text(feature_ptr->source_file_path.string());
-
-        ImGui::TableNextRow();
-        ImGui::TableSetColumnIndex(0);
-        ImGui::Text("Description:");
-        ImGui::TableSetColumnIndex(1);
-        display_formatted_text(feature_ptr->description);
-
-        ImGui::EndTable();
-    }
-    ImGui::PopStyleVar();
+    end_content_table();
 }
 
 void dnd::DisplayVisitor::visit(const Choosable* choosable_ptr) {
-    std::string table_id = fmt::format("{}_table", choosable_ptr->name);
-    ImGui::PushStyleVar(ImGuiStyleVar_CellPadding, cell_padding);
-    if (ImGui::BeginTable(table_id.c_str(), 2, content_table_flags)) {
-        ImGui::TableSetupColumn("Labels", ImGuiTableColumnFlags_WidthFixed, first_column_width);
-        ImGui::TableSetupColumn("Values");
+    begin_content_table(choosable_ptr);
 
-        ImGui::TableNextRow();
-        ImGui::TableSetColumnIndex(0);
-        ImGui::Text("Type:");
-        ImGui::TableSetColumnIndex(1);
-        ImGui::Text("Choosable");
+    label("Type:");
+    ImGui::Text("Choosable");
+    source(choosable_ptr);
+    label("Description:");
+    display_formatted_text(choosable_ptr->description);
 
-        ImGui::TableNextRow();
-        ImGui::TableSetColumnIndex(0);
-        ImGui::Text("Source:");
-        ImGui::TableSetColumnIndex(1);
-        display_formatted_text(choosable_ptr->source_file_path.string());
-
-        ImGui::TableNextRow();
-        ImGui::TableSetColumnIndex(0);
-        ImGui::Text("Description:");
-        ImGui::TableSetColumnIndex(1);
-        display_formatted_text(choosable_ptr->description);
-
-        ImGui::EndTable();
-    }
-    ImGui::PopStyleVar();
+    end_content_table();
 }
 
 void dnd::DisplayVisitor::display_formatted_text(const std::string& formatted_text) {
