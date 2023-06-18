b79eecb10aebf27857bd146c0aeb6a3a63dff9f7
Friedrich Tschirpke <friedrich.tschirpke@student.hu-berlin.de>
2023-06-18 19:05:25 +0200

    refactor: character race parser

---
+++ src/core/parsing/character_race/character_race_parser.cpp
@@ -0,0 +1,37 @@
+#include <dnd_config.hpp>
+
+#include "character_race_parser.hpp"
+
+#include <filesystem>
+#include <string>
+#include <utility>
+#include <vector>
+
+#include <nlohmann/json.hpp>
+
+#include <core/errors/errors.hpp>
+#include <core/errors/parsing_error.hpp>
+#include <core/parsing/feature/feature_parser.hpp>
+#include <core/parsing/parser.hpp>
+#include <core/validation/character_race/character_race_data.hpp>
+
+dnd::CharacterRaceParser::CharacterRaceParser(const std::filesystem::path& filepath) noexcept
+    : Parser(filepath), feature_parser(filepath) {}
+
+dnd::Errors dnd::CharacterRaceParser::parse(nlohmann::ordered_json&& json, CharacterRaceData& data) const {
+    Errors errors;
+    if (!json.is_object()) {
+        errors.add_parsing_error(
+            ParsingErrorCode::INVALID_FILE_FORMAT, get_filepath(), "The character race json is not an object."
+        );
+        return errors;
+    }
+
+    errors += parse_required_attribute(json, "name", data.name);
+    errors += parse_required_attribute(json, "description", data.description);
+    data.source_path = get_filepath();
+
+    errors += parse_required_attribute(json, "has_subraces", data.subraces);
+
+    errors += feature_parser.parse_multiple(std::move(json), data.features_data, &data);
+}
