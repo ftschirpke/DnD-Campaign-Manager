0583036d2e79b247039bd6ea69fc45d4a4155632
Friedrich Tschirpke <friedrich.tschirpke@student.hu-berlin.de>
2023-06-18 18:24:48 +0200

    refactor: parse list of features

--- src/core/parsing/feature/feature_parser.cpp
+++ src/core/parsing/feature/feature_parser.cpp
@@ -46,3 +46,24 @@ dnd::Errors dnd::FeatureParser::parse(nlohmann::ordered_json&& json, FeatureData
     errors += effect_holder_parser.parse(std::move(json), data.main_part_data);
     return errors;
 }
+
+dnd::Errors dnd::FeatureParser::parse_multiple(
+    nlohmann::ordered_json&& json, std::vector<FeatureData>& data, const dnd::ValidationData* parent
+) const {
+    Errors errors;
+    if (!json.is_object()) {
+        errors.add_parsing_error(
+            ParsingErrorCode::INVALID_FILE_FORMAT, get_filepath(), "The feature json is not an object."
+        );
+        return errors;
+    }
+
+    for (auto& [feature_name, feature_json] : json.items()) {
+        FeatureData& feature_data = data.emplace_back(parent);
+        feature_data.name = feature_name;
+        feature_data.source_path = get_filepath();
+        errors += parse(std::move(feature_json), feature_data);
+    }
+
+    return errors;
+}
--- src/core/parsing/feature/feature_parser.hpp
+++ src/core/parsing/feature/feature_parser.hpp
@@ -4,6 +4,7 @@
 #include <dnd_config.hpp>
 
 #include <filesystem>
+#include <vector>
 
 #include <nlohmann/json.hpp>
 
@@ -11,6 +12,8 @@
 #include <core/parsing/effect_holder/effect_holder_parser.hpp>
 #include <core/parsing/parser.hpp>
 #include <core/validation/feature/feature_data.hpp>
+#include <core/validation/validation_data.hpp>
+
 namespace dnd {
 
 class FeatureParser : public Parser {
@@ -23,6 +26,14 @@ public:
      * @return the errors that occured during parsing
      */
     Errors parse(nlohmann::ordered_json&& json, FeatureData& data) const;
+    /**
+     * @brief Parses the data from the json into the data objects
+     * @param json the json to parse
+     * @param data the data objects to parse into
+     * @return the errors that occured during parsing
+     */
+    Errors parse_multiple(nlohmann::ordered_json&& json, std::vector<FeatureData>& data, const ValidationData* parent)
+        const;
 private:
     EffectHolderParser effect_holder_parser;
 };
