67799f15245442679366f2d1cbc31500d0f90a56
Friedrich Tschirpke <friedrich.tschirpke@student.hu-berlin.de>
2023-06-23 20:20:52 +0200

    test: errors

---
+++ tests/core/errors/parsing_error_test.cpp
@@ -0,0 +1,39 @@
+#include <core/errors/parsing_error.hpp>
+
+#include <catch2/catch_test_macros.hpp>
+
+#include <filesystem>
+#include <string>
+
+static constexpr const char* tags = "[core/errors/parsing_error]";
+
+TEST_CASE("dnd::ParsingError: basic getters", tags) {
+    std::filesystem::path filepath = "/path/to/file.txt";
+    std::string error_message = "An error occurred";
+
+    SECTION("Get error code") {
+        dnd::ParsingError error1(dnd::ParsingErrorCode::FILE_NOT_FOUND, filepath, error_message);
+        REQUIRE(error1.get_error_code() == dnd::ParsingErrorCode::FILE_NOT_FOUND);
+
+        dnd::ParsingError error2(dnd::ParsingErrorCode::INVALID_FILE_FORMAT, filepath, error_message);
+        REQUIRE(error2.get_error_code() == dnd::ParsingErrorCode::INVALID_FILE_FORMAT);
+    }
+
+    SECTION("Get file path") {
+        dnd::ParsingError error1(dnd::ParsingErrorCode::MISSING_ATTRIBUTE, filepath, error_message);
+        REQUIRE(error1.get_filepath() == filepath);
+
+        std::filesystem::path other_filepath = "/another/path/file.txt";
+        dnd::ParsingError error2(dnd::ParsingErrorCode::INVALID_ATTRIBUTE_TYPE, other_filepath, error_message);
+        REQUIRE(error2.get_filepath() == other_filepath);
+    }
+
+    SECTION("Get error message") {
+        dnd::ParsingError error1(dnd::ParsingErrorCode::UNEXPECTED_ATTRIBUTE, filepath, error_message);
+        REQUIRE(error1.get_error_message() == error_message);
+
+        std::string other_message = "Another error occurred";
+        dnd::ParsingError error2(dnd::ParsingErrorCode::UNKNOWN_ERROR, filepath, other_message);
+        REQUIRE(error2.get_error_message() == other_message);
+    }
+}
---
+++ tests/core/errors/validation_error_test.cpp
@@ -0,0 +1,41 @@
+#include <core/errors/validation_error.hpp>
+
+#include <catch2/catch_test_macros.hpp>
+
+#include <core/validation/character/character_data.hpp>
+#include <core/validation/feature/choosable_feature_data.hpp>
+
+static constexpr const char* tags = "[core/errors/validation_error]";
+
+TEST_CASE("dnd::ValidationError: basic getters", tags) {
+    const dnd::CharacterData validation_data;
+    const dnd::ChoosableFeatureData other_validation_data;
+    const std::string error_message = "Error message";
+
+    SECTION("Get error code") {
+        dnd::ValidationError error1(dnd::ValidationErrorCode::MISSING_ATTRIBUTE, &validation_data, error_message);
+        REQUIRE(error1.get_error_code() == dnd::ValidationErrorCode::MISSING_ATTRIBUTE);
+
+        dnd::ValidationError error2(
+            dnd::ValidationErrorCode::INVALID_ATTRIBUTE_FORMAT, &validation_data, error_message
+        );
+        REQUIRE(error2.get_error_code() == dnd::ValidationErrorCode::INVALID_ATTRIBUTE_FORMAT);
+    }
+
+    SECTION("Get validation data") {
+        dnd::ValidationError error1(dnd::ValidationErrorCode::INVALID_ATTRIBUTE_VALUE, &validation_data, error_message);
+        REQUIRE(*dynamic_cast<const dnd::CharacterData*>(error1.get_validation_data()) == validation_data);
+
+        dnd::ValidationError error2(dnd::ValidationErrorCode::INVALID_RELATION, &other_validation_data, error_message);
+        REQUIRE(*dynamic_cast<const dnd::ChoosableFeatureData*>(error2.get_validation_data()) == other_validation_data);
+    }
+
+    SECTION("Get error message") {
+        dnd::ValidationError error1(dnd::ValidationErrorCode::INCONSISTENT_ATTRIBUTES, &validation_data, error_message);
+        REQUIRE(error1.get_error_message() == error_message);
+
+        std::string other_message = "Another error message";
+        dnd::ValidationError error2(dnd::ValidationErrorCode::UNKNOWN_ERROR, &validation_data, other_message);
+        REQUIRE(error2.get_error_message() == other_message);
+    }
+}
