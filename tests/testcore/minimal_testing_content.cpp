9e3952b720206c6f14fe2c6565417c5eee3f9406
Friedrich Tschirpke <friedrich.tschirpke@student.hu-berlin.de>
2023-09-20 00:03:32 +0200

    test: character data

--- tests/testcore/minimal_testing_content.cpp
+++ tests/testcore/minimal_testing_content.cpp
@@ -12,6 +12,7 @@
 #include <core/models/character_subclass/character_subclass.hpp>
 #include <core/models/character_subrace/character_subrace.hpp>
 #include <core/models/spell/spell.hpp>
+#include <core/validation/character/character_data.hpp>
 #include <core/validation/character_class/character_class_data.hpp>
 #include <core/validation/character_race/character_race_data.hpp>
 #include <core/validation/spell/spell_data.hpp>
@@ -89,6 +90,18 @@ static void add_classes(dnd::Content& content) {
     assert(class_data.validate_relations(content).ok());
     content.add_character_class(dnd::CharacterClass::create(std::move(class_data), content));
 
+    dnd::CharacterClassData class_data2;
+    dndtest::set_valid_mock_values(class_data2, "Rogue");
+    class_data2.spellcasting_data.is_spellcaster = false;
+    auto& feature_data2 = class_data2.features_data.emplace_back(&class_data2);
+    dndtest::set_valid_mock_values(feature_data2, "Example Class Feature 2");
+    class_data2.subclass_feature_name = "Example Class Feature 2";
+    class_data2.hit_dice_data.str = "d8";
+    class_data2.important_levels_data.feat_levels = {4, 8, 12, 16, 19};
+    assert(class_data2.validate().ok());
+    assert(class_data2.validate_relations(content).ok());
+    content.add_character_class(dnd::CharacterClass::create(std::move(class_data2), content));
+
     dnd::CharacterSubclassData subclass_data;
     dndtest::set_valid_mock_values(subclass_data, "Abjuration Wizard");
     subclass_data.spellcasting_data.is_spellcaster = false;
@@ -98,6 +111,16 @@ static void add_classes(dnd::Content& content) {
     assert(subclass_data.validate().ok());
     assert(subclass_data.validate_relations(content).ok());
     content.add_character_subclass(dnd::CharacterSubclass::create(std::move(subclass_data), content));
+
+    dnd::CharacterSubclassData subclass_data2;
+    dndtest::set_valid_mock_values(subclass_data2, "Assassin");
+    subclass_data2.spellcasting_data.is_spellcaster = false;
+    auto& feature_data4 = subclass_data2.features_data.emplace_back(&subclass_data2);
+    dndtest::set_valid_mock_values(feature_data4, "Example Subclass Feature 2");
+    subclass_data2.class_name = "Rogue";
+    assert(subclass_data2.validate().ok());
+    assert(subclass_data2.validate_relations(content).ok());
+    content.add_character_subclass(dnd::CharacterSubclass::create(std::move(subclass_data2), content));
 }
 
 static void add_races(dnd::Content& content) {
@@ -119,14 +142,51 @@ static void add_races(dnd::Content& content) {
     assert(race_data2.validate_relations(content).ok());
     content.add_character_race(dnd::CharacterRace::create(std::move(race_data2), content));
 
-    dnd::CharacterSubraceData subrace_data;
-    dndtest::set_valid_mock_values(subrace_data, "Hill Dwarf");
-    auto& feature_data3 = subrace_data.features_data.emplace_back(&subrace_data);
-    dndtest::set_valid_mock_values(feature_data3, "Example Subrace Feature");
-    subrace_data.race_name = "Dwarf";
-    assert(subrace_data.validate().ok());
-    assert(subrace_data.validate_relations(content).ok());
-    content.add_character_subrace(dnd::CharacterSubrace::create(std::move(subrace_data), content));
+    // dnd::CharacterRaceData race_data3;
+    // dndtest::set_valid_mock_values(race_data3, "Elf");
+    // auto& feature_data3 = race_data3.features_data.emplace_back(&race_data3);
+    // dndtest::set_valid_mock_values(feature_data3, "Example Race Feature 3");
+    // race_data3.subraces = true;
+    // assert(race_data3.validate().ok());
+    // assert(race_data3.validate_relations(content).ok());
+    // content.add_character_race(dnd::CharacterRace::create(std::move(race_data3), content));
+
+    dnd::CharacterSubraceData subrace_data1;
+    dndtest::set_valid_mock_values(subrace_data1, "Hill Dwarf");
+    auto& feature_data4 = subrace_data1.features_data.emplace_back(&subrace_data1);
+    dndtest::set_valid_mock_values(feature_data4, "Example Subrace Feature");
+    subrace_data1.race_name = "Dwarf";
+    assert(subrace_data1.validate().ok());
+    assert(subrace_data1.validate_relations(content).ok());
+    content.add_character_subrace(dnd::CharacterSubrace::create(std::move(subrace_data1), content));
+
+    // dnd::CharacterSubraceData subrace_data2;
+    // dndtest::set_valid_mock_values(subrace_data2, "High Elf");
+    // auto& feature_data5 = subrace_data2.features_data.emplace_back(&subrace_data2);
+    // dndtest::set_valid_mock_values(feature_data5, "Example Subrace Feature 2");
+    // subrace_data2.race_name = "Elf";
+    // assert(subrace_data2.validate().ok());
+    // assert(subrace_data2.validate_relations(content).ok());
+    // content.add_character_subrace(dnd::CharacterSubrace::create(std::move(subrace_data2), content));
+}
+
+static void add_characters(dnd::Content& content) {
+    DND_UNUSED(content);
+    // dnd::CharacterData character_data;
+    // dndtest::set_valid_mock_values(character_data, "Example Character");
+    // auto& feature_data = character_data.features_data.emplace_back(&character_data);
+    // dndtest::set_valid_mock_values(feature_data, "Example Character Feature");
+    // character_data.base_ability_scores_data.ability_scores = {10, 8, 12, 15, 13, 14};
+    // character_data.character_basis_data.race_name = "Dwarf";
+    // character_data.character_basis_data.subrace_name = "Hill Dwarf";
+    // character_data.character_basis_data.class_name = "Wizard";
+    // character_data.character_basis_data.subclass_name = "Abjuration Wizard";
+    // character_data.progression_data.level = 4;
+    // character_data.progression_data.xp = 4200;
+    // character_data.progression_data.hit_dice_rolls = {6, 4, 2, 5};
+    // assert(character_data.validate().ok());
+    // assert(character_data.validate_relations(content).ok());
+    // content.add_character(dnd::Character::create(std::move(character_data), content));
 }
 
 dnd::Content dndtest::minimal_testing_content() {
@@ -136,6 +196,7 @@ dnd::Content dndtest::minimal_testing_content() {
     add_spells(content);
     add_classes(content);
     add_races(content);
+    add_characters(content);
 
     return content;
 }
