c57cf40ec0cdfbc06a08e46665af28f68dda5e60
Friedrich Tschirpke <friedrich.tschirpke@student.hu-berlin.de>
2023-09-24 01:39:06 +0200

    refactor: extract TrieSearchPath into own file

--- src/core/searching/trie_search/trie_content_search.hpp
+++ src/core/searching/trie_search/trie_content_search.hpp
@@ -19,52 +19,9 @@
 #include <core/models/item/item.hpp>
 #include <core/models/spell/spell.hpp>
 #include <core/searching/trie_search/trie.hpp>
+#include <core/searching/trie_search/trie_search_path.hpp>
 
 namespace dnd {
-
-template <typename T>
-class TrieSearchPath : public std::stack<const TrieNode<T>*> {
-public:
-    /**
-     * @brief Push the child of the top node onto the stack if it exists (otherwise push nullptr)
-     * @param c the character to get the child for
-     */
-    void push_top_child(char c);
-    /**
-     * @brief Insert the successors of the top node into a vector if such successors exist
-     * @tparam S
-     * @param vector the vector to insert the successors into
-     * @return "true" if successors were inserted, "false" otherwise (nothing inserted)
-     */
-    template <typename S>
-    requires std::derived_from<T, S>
-    bool insert_top_successors_into(std::vector<S*>& vec) const;
-};
-
-template <typename T>
-void TrieSearchPath<T>::push_top_child(char c) {
-    if (this->top() == nullptr) {
-        this->push(nullptr);
-    } else {
-        this->push(this->top()->get_child(c));
-    }
-}
-
-template <typename T>
-template <typename S>
-requires std::derived_from<T, S>
-bool TrieSearchPath<T>::insert_top_successors_into(std::vector<S*>& vec) const {
-    if (this->top() == nullptr) {
-        return false;
-    }
-    std::unordered_set<T*> successors_set = this->top()->successors();
-    if (successors_set.empty()) {
-        return false;
-    }
-    vec.insert(vec.end(), successors_set.begin(), successors_set.end());
-    return true;
-}
-
 
 /**
  * @brief A class representing a content search query
---
+++ src/core/searching/trie_search/trie_search_path.hpp
@@ -0,0 +1,53 @@
+#ifndef TRIE_SEARCH_PATH_HPP_
+#define TRIE_SEARCH_PATH_HPP_
+
+#include <dnd_config.hpp>
+
+namespace dnd {
+
+template <typename T>
+class TrieSearchPath : public std::stack<const TrieNode<T>*> {
+public:
+    /**
+     * @brief Push the child of the top node onto the stack if it exists (otherwise push nullptr)
+     * @param c the character to get the child for
+     */
+    void push_top_child(char c);
+    /**
+     * @brief Insert the successors of the top node into a vector if such successors exist
+     * @tparam S
+     * @param vector the vector to insert the successors into
+     * @return "true" if successors were inserted, "false" otherwise (nothing inserted)
+     */
+    template <typename S>
+    requires std::derived_from<T, S>
+    bool insert_top_successors_into(std::vector<S*>& vec) const;
+};
+
+template <typename T>
+void TrieSearchPath<T>::push_top_child(char c) {
+    if (this->top() == nullptr) {
+        this->push(nullptr);
+    } else {
+        this->push(this->top()->get_child(c));
+    }
+}
+
+template <typename T>
+template <typename S>
+requires std::derived_from<T, S>
+bool TrieSearchPath<T>::insert_top_successors_into(std::vector<S*>& vec) const {
+    if (this->top() == nullptr) {
+        return false;
+    }
+    std::unordered_set<T*> successors_set = this->top()->successors();
+    if (successors_set.empty()) {
+        return false;
+    }
+    vec.insert(vec.end(), successors_set.begin(), successors_set.end());
+    return true;
+}
+
+} // namespace dnd
+
+#endif // TRIE_SEARCH_PATH_HPP_
