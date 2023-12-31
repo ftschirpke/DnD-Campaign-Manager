57c1cca61d499353a1471b8d41486961a5bf090a
Friedrich Tschirpke <friedrich.tschirpke@student.hu-berlin.de>
2023-12-31 05:39:41 +0100

    fix: move data result to header only and references explicit

--- src/core/utils/data_result.hpp
+++ src/core/utils/data_result.hpp
@@ -24,9 +24,9 @@ public:
     static DataResult invalid(DataT&& data, Errors&& errors) noexcept;
 
     bool is_valid() const noexcept;
-    const OutputT& value() const;
+    const OutputT& value_ref() const;
     OutputT&& value();
-    const std::pair<DataT, Errors>& data_and_errors() const;
+    const std::pair<DataT, Errors>& data_and_errors_ref() const;
     std::pair<DataT, Errors>&& data_and_errors();
 private:
     DataResult(tl::expected<OutputT, std::pair<DataT, Errors>>&& inner) noexcept;
@@ -50,11 +50,75 @@ CreateResult<T> InvalidCreate(typename T::Data&& data, Errors&& errors) noexcept
 template <typename T>
 using FactoryResult = DataResult<std::unique_ptr<T>, typename T::Data>;
 
-template <typename T>
-FactoryResult<T> ValidFactory(std::unique_ptr<T>&& output) noexcept;
+template <typename T, typename SubT>
+requires std::is_base_of_v<T, SubT>
+FactoryResult<T> ValidFactory(std::unique_ptr<SubT>&& output) noexcept;
 
 template <typename T>
 FactoryResult<T> InvalidFactory(typename T::Data&& data, Errors&& errors) noexcept;
+
+
+// === IMPLEMENTATION ===
+
+template <typename OutputT, typename DataT>
+DataResult<OutputT, DataT> DataResult<OutputT, DataT>::valid(OutputT&& output) noexcept {
+    return DataResult(tl::expected<OutputT, std::pair<DataT, Errors>>(std::move(output)));
+}
+
+template <typename OutputT, typename DataT>
+DataResult<OutputT, DataT> DataResult<OutputT, DataT>::invalid(DataT&& data, Errors&& errors) noexcept {
+    return DataResult(tl::unexpected(std::make_pair(std::move(data), std::move(errors))));
+}
+
+template <typename OutputT, typename DataT>
+bool DataResult<OutputT, DataT>::is_valid() const noexcept {
+    return inner.has_value();
+}
+
+template <typename OutputT, typename DataT>
+const OutputT& DataResult<OutputT, DataT>::value_ref() const {
+    return inner.value();
+}
+
+template <typename OutputT, typename DataT>
+OutputT&& DataResult<OutputT, DataT>::value() {
+    return std::move(inner.value());
+}
+
+template <typename OutputT, typename DataT>
+const std::pair<DataT, Errors>& DataResult<OutputT, DataT>::data_and_errors_ref() const {
+    return inner.error();
+}
+
+template <typename OutputT, typename DataT>
+std::pair<DataT, Errors>&& DataResult<OutputT, DataT>::data_and_errors() {
+    return std::move(inner.error());
+}
+
+template <typename OutputT, typename DataT>
+DataResult<OutputT, DataT>::DataResult(tl::expected<OutputT, std::pair<DataT, Errors>>&& inner) noexcept
+    : inner(std::move(inner)) {}
+
+
+template <typename T>
+CreateResult<T> ValidCreate(T&& output) noexcept {
+    return CreateResult<T>::valid(std::move(output));
+}
+
+template <typename T>
+CreateResult<T> InvalidCreate(typename T::Data&& data, Errors&& errors) noexcept {
+    return CreateResult<T>::invalid(std::move(data), std::move(errors));
+}
+
+template <typename T>
+FactoryResult<T> ValidFactory(std::unique_ptr<T>&& output) noexcept {
+    return FactoryResult<T>::valid(std::move(output));
+}
+
+template <typename T>
+FactoryResult<T> InvalidFactory(typename T::Data&& data, Errors&& errors) noexcept {
+    return FactoryResult<T>::invalid(std::move(data), std::move(errors));
+}
 
 } // namespace dnd
 
