08172ad82113edd84e262ffb6223f3ec3c928b9b
Friedrich Tschirpke <friedrich.tschirpke@student.hu-berlin.de>
2024-01-12 21:08:28 +0100

    feat: introduce implied values

--- src/core/models/character/stats.cpp
+++ src/core/models/character/stats.cpp
@@ -1,19 +1,48 @@
-#include <algorithm>
 #include <dnd_config.hpp>
 
-#include "core/basic_mechanics/skills.hpp"
 #include "stats.hpp"
 
+#include <algorithm>
 #include <cassert>
 #include <string>
 #include <unordered_map>
 
-dnd::Stats dnd::Stats::create_default() noexcept {
+#include <fmt/format.h>
+
+#include <core/basic_mechanics/abilities.hpp>
+#include <core/basic_mechanics/skills.hpp>
+
+namespace dnd {
+
+static int calculate_modifier(int score) noexcept { return score / 2 - 5; }
+
+Stats Stats::create_default() noexcept {
     Stats stats;
     return stats;
 }
 
-dnd::Stats::Stats() noexcept {
+Stats Stats::create_from_base_scores_and_stat_changes(
+    const AbilityScores& base_ability_scores, std::vector<CRef<StatChange>> stat_changes
+) noexcept {
+    Stats stats;
+    stats.mutable_values["STR"] = base_ability_scores.get_strength();
+    stats.mutable_values["DEX"] = base_ability_scores.get_dexterity();
+    stats.mutable_values["CON"] = base_ability_scores.get_constitution();
+    stats.mutable_values["INT"] = base_ability_scores.get_intelligence();
+    stats.mutable_values["WIS"] = base_ability_scores.get_wisdom();
+    stats.mutable_values["CHA"] = base_ability_scores.get_charisma();
+
+    // TODO: Apply all stat changes for the ability scores
+
+    stats.calculate_ability_modifiers();
+    stats.calculate_skill_modifiers();
+
+    // TODO: Apply all other stat changes
+
+    return stats;
+}
+
+Stats::Stats() noexcept {
     mutable_values["STR"] = 10;
     mutable_values["DEX"] = 10;
     mutable_values["CON"] = 10;
@@ -22,7 +51,7 @@ dnd::Stats::Stats() noexcept {
     mutable_values["CHA"] = 10;
 }
 
-bool dnd::Stats::is_complete() const noexcept {
+bool Stats::is_complete() const noexcept {
     assert(mutable_values.contains("STR"));
     assert(mutable_values.contains("DEX"));
     assert(mutable_values.contains("CON"));
@@ -32,9 +61,34 @@ bool dnd::Stats::is_complete() const noexcept {
     bool has_ability_modifiers = mutable_values.contains("STR_MOD") && mutable_values.contains("DEX_MOD")
                                  && mutable_values.contains("CON_MOD") && mutable_values.contains("INT_MOD")
                                  && mutable_values.contains("WIS_MOD") && mutable_values.contains("CHA_MOD");
-    auto all_skills = get_all_skills();
+    std::vector<std::string> all_skills = get_all_skills();
     bool has_skill_modifiers = std::all_of(all_skills.begin(), all_skills.end(), [this](const std::string& skill) {
         return mutable_values.contains(skill);
     });
     return has_ability_modifiers && has_skill_modifiers;
 }
+
+void Stats::calculate_ability_modifiers() noexcept {
+    assert(mutable_values.contains("STR"));
+    implied_values["STR_MOD"] = (mutable_values["STR"] - 10) / 2;
+    assert(mutable_values.contains("DEX"));
+    implied_values["DEX_MOD"] = (mutable_values["DEX"] - 10) / 2;
+    assert(mutable_values.contains("CON"));
+    implied_values["CON_MOD"] = (mutable_values["CON"] - 10) / 2;
+    assert(mutable_values.contains("INT"));
+    implied_values["INT_MOD"] = (mutable_values["INT"] - 10) / 2;
+    assert(mutable_values.contains("WIS"));
+    implied_values["WIS_MOD"] = (mutable_values["WIS"] - 10) / 2;
+    assert(mutable_values.contains("CHA"));
+    implied_values["CHA_MOD"] = (mutable_values["CHA"] - 10) / 2;
+}
+
+void Stats::calculate_skill_modifiers() noexcept {
+    for (const auto& [skill, ability] : get_abilities_for_all_skills()) {
+        const std::string ability_modifier = ability + "_MOD";
+        assert(implied_values.contains(ability_modifier));
+        mutable_values[skill] = implied_values[ability_modifier];
+    }
+}
+
+} // namespace dnd
--- src/core/models/character/stats.hpp
+++ src/core/models/character/stats.hpp
@@ -5,19 +5,31 @@
 
 #include <string>
 #include <unordered_map>
+#include <vector>
+
+#include <core/models/character/ability_scores.hpp>
+#include <core/models/effects/stat_change/stat_change.hpp>
+#include <core/utils/types.hpp>
 
 namespace dnd {
 
 class Stats {
 public:
     static Stats create_default() noexcept;
+    static Stats create_from_base_scores_and_stat_changes(
+        const AbilityScores& base_ability_scores, std::vector<CRef<StatChange>> stat_changes
+    ) noexcept;
     Stats() noexcept;
 
     bool is_complete() const noexcept;
-    void derive_implied_values() noexcept;
 private:
-    std::unordered_map<std::string, int> constant_values;
+    void derive_values_from_ability_scores() noexcept;
+    void calculate_ability_modifiers() noexcept;
+    void calculate_skill_modifiers() noexcept;
+
+    std::unordered_map<std::string, const int> constant_values;
     std::unordered_map<std::string, int> mutable_values;
+    std::unordered_map<std::string, int> implied_values;
 };
 
 } // namespace dnd
