306add7cd2a9f3c5290cb2d8780d2524709a2825
Friedrich Tschirpke <friedrich.tschirpke@live.de>
2024-08-06 15:23:36 +0200

    fix: add noexcept to search result constructors

--- src/core/searching/search_result.hpp
+++ src/core/searching/search_result.hpp
@@ -9,15 +9,15 @@ namespace dnd {
 
 class SearchResult {
 public:
-    SearchResult() = default;
-    SearchResult(const ContentPiece* content_piece_ptr, int64_t significance);
+    SearchResult() noexcept = default;
+    SearchResult(const ContentPiece* content_piece_ptr, int64_t significance) noexcept;
 
     const ContentPiece* content_piece_ptr;
     int64_t significance;
 };
 
 
-inline SearchResult::SearchResult(const ContentPiece* content_piece_ptr, int64_t significance)
+inline SearchResult::SearchResult(const ContentPiece* content_piece_ptr, int64_t significance) noexcept
     : content_piece_ptr(content_piece_ptr), significance(significance) {}
 
 } // namespace dnd
