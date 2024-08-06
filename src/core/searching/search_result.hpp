6cf876bfa43ca2989abdb378c95b8f71970bef7f
Friedrich Tschirpke <friedrich.tschirpke@live.de>
2024-08-06 15:20:50 +0200

    fix: search result constructors

--- src/core/searching/search_result.hpp
+++ src/core/searching/search_result.hpp
@@ -7,10 +7,18 @@
 
 namespace dnd {
 
-struct SearchResult {
+class SearchResult {
+public:
+    SearchResult() = default;
+    SearchResult(const ContentPiece* content_piece_ptr, int64_t significance);
+
     const ContentPiece* content_piece_ptr;
     int64_t significance;
 };
+
+
+inline SearchResult::SearchResult(const ContentPiece* content_piece_ptr, int64_t significance)
+    : content_piece_ptr(content_piece_ptr), significance(significance) {}
 
 } // namespace dnd
 
