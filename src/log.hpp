647423f9047c7a90fee40193983be085f304f638
Friedrich Tschirpke <friedrich.tschirpke@live.de>
2025-04-18 00:13:38 +0200

    fix: disable logging in Release mode without compile warnings

--- src/log.hpp
+++ src/log.hpp
@@ -7,8 +7,6 @@
 
 namespace dnd {
 
-#if DND_DEBUG_MODE
-
 class Logger {
 public:
     Logger(const char* log_type, const char* file, const char* func, int line);
@@ -23,12 +21,17 @@ private:
     int line;
 };
 
+
 inline Logger::Logger(const char* log_type, const char* file, const char* func, int line)
     : log_type(log_type), file(file), func(func), line(line) {}
 
 template <typename... T>
 inline void Logger::operator()(fmt::format_string<T...> fmt, T&&... args) {
+#if DND_DEBUG_MODE
     log_helper(fmt, fmt::make_format_args(args...));
+#else
+    DND_UNUSED(fmt, args...);
+#endif
 }
 
 inline void Logger::log_helper(fmt::string_view fmt, fmt::format_args args) {
@@ -36,12 +39,6 @@ inline void Logger::log_helper(fmt::string_view fmt, fmt::format_args args) {
 }
 
 #define _DND_LOG(log_type) Logger(log_type, __FILE__, __func__, __LINE__)
-
-#else
-
-#define _DND_LOG(log_type) DND_UNUSED
-
-#endif
 
 #define LOGINFO _DND_LOG("INFO")
 #define LOGDEBUG _DND_LOG("DEBUG")
