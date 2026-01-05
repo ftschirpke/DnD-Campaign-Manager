035ee96ce5267b994446bad1771a7a45f3cdce1d
Friedrich Tschirpke <friedrich.tschirpke@live.de>
2026-01-06 00:46:49 +0100

    refactor: X pattern in fuzzy content search

--- src/core/searching/fuzzy_search/fuzzy_content_search.hpp
+++ src/core/searching/fuzzy_search/fuzzy_content_search.hpp
@@ -3,39 +3,30 @@
 
 #include <dnd_config.hpp>
 
-#include <compare>
 #include <string>
 
-#include <core/content.hpp>
 #include <core/searching/search_result.hpp>
+#include <x/content_pieces.hpp>
 
 namespace dnd {
+
+class Content;
 
 constexpr uint32_t FUZZY_SEARCH_MINIMUM_QUERY_LENGTH = 1;
 
 struct FuzzySearchOptions {
-    bool search_characters;
-    bool search_classes;
-    bool search_subclasses;
-    bool search_species;
-    bool search_subspecies;
-    bool search_items;
-    bool search_spells;
+#define X(C, U, j, a, p, P) bool search_##p;
+    X_OWNED_CONTENT_PIECES
+#undef X
     bool search_features;
-    bool search_choosables;
 
     std::strong_ordering operator<=>(const FuzzySearchOptions&) const = default;
 
     void set_all(bool value) {
-        search_characters = value;
-        search_classes = value;
-        search_subclasses = value;
-        search_species = value;
-        search_subspecies = value;
-        search_items = value;
-        search_spells = value;
+#define X(C, U, j, a, p, P) search_##p = value;
+        X_OWNED_CONTENT_PIECES
+#undef X
         search_features = value;
-        search_choosables = value;
     }
 };
 
